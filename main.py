d336aea5fcd322a1ac85eef5026141677303362b
Sarah <happylucylocket123@hotmail.com>
2023-06-06 13:20:58 -0700

    Make it not have duplicate comments

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import praw
 import re
 import math
+import time
 
 r = praw.Reddit('bot1')
-
-subreddit = r.subreddit("testingground4bots")
+subreddit = r.subreddit("vancouver")
 
 gst = 0.05
 hotdogPrice = 150 * gst + 150
-
 
 def hotdogRes(signPrice):
     price = float(signPrice.replace("$", ""))*100
@@ -16,11 +15,23 @@ def hotdogRes(signPrice):
     s = ("You can buy " + str(amountOfHotdogs) + " Costco hotdog + drink combos with " + str(signPrice) + " (including 5% GST)!")
     return s
 
-for c in subreddit.stream.comments():
+
+for c in subreddit.stream.comments(skip_existing=True):
+    # Read the already commented IDs
+    f = open('idsResponded.txt', 'r+')
+    ids = f.readlines()
+
     match = re.search(r'/u/CostcoHotdogBot', c.body) and re.search(r'\$\d+(\.\d{2})?', c.body)
-    if (match):
+    hasNotCommented = str(c.id) + '\n' not in ids
+    if (hasNotCommented) and (match):
+        print("MATCH FOUND\n")
         price = match.group()
         message = hotdogRes(price)
         c.reply(message)
+        # Record the id of the comment the bot is leaving
+        f.write(str(c.id) + '\n')
+    elif (not hasNotCommented) and (match):
+        print("ALREADY COMMENTED\n")
     else:
         print("NO MATCH\n")
+    time.sleep(30)
