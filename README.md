75f202cac38b0c4f5c4d5e82930f1761ed7dbeaf
Sarah <happylucylocket123@hotmail.com>
2023-06-06 14:13:30 -0700

    Update README and comments

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # costcohotdogbot
+Replies to comments in /r/vancouver that contains a money value ($12.34, $23, etc.) and mentions `/u/CostcoHotdogBot`.
--- main.py
+++ main.py
@@ -10,18 +10,21 @@ gst = 0.05
 hotdogPrice = 150 * gst + 150
 
 def hotdogRes(signPrice):
+    """
+        Calculates the amount of hotdog combos purchasable from an amount of money
+    """
     price = float(signPrice.replace("$", ""))*100
     amountOfHotdogs = math.floor(price/hotdogPrice)
     s = ("You can buy " + str(amountOfHotdogs) + " Costco hotdog + drink combos with " + str(signPrice) + " (including 5% GST)!")
     return s
-
 
 for c in subreddit.stream.comments(skip_existing=True):
     # Read the already commented IDs
     f = open('idsResponded.txt', 'r+')
     ids = f.readlines()
 
-    match = re.search(r'/u/CostcoHotdogBot', c.body) and re.search(r'\$\d+(\.\d{2})?', c.body)
+    # Check for comments that contain a price value with $ and /u/CostcoHotdogBot
+    match = re.search(r'\/?u\/CostcoHotdogBot', c.body) and re.search(r'\$\d+(\.\d{2})?', c.body)
     hasNotCommented = str(c.id) + '\n' not in ids
     if (hasNotCommented) and (match):
         print("MATCH FOUND\n")
